92dd23468a13bc14158f315c4fb17c02070ced53
austinmscholl <asprof34@gmail.com>
2019-03-25 12:13:45 -0400

    added blog

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,15 +14,15 @@ class About extends React.Component{
                 <h4 className="ah">About Me</h4>
                 <p className="ap">Graduate of <a className="nodecoration" href="https://elevenfifty.org" target="_blank">Eleven Fifty's</a> JavaScript bootcamp</p>
                 {/* <p>Founder/Lead Developer at <a className="nodecoration" href="https://www.unfoldingsoftware.com" target="_blank">Unfolding Software LLC</a></p> */}
-                <p className="ap">I am currently looking for work, and a Salesforce Pathfinder</p>
+                <p className="ap">Currently a <a className="nodecoration" href="https://pathfinder.salesforce.com/s/" target="_blank">Salesforce Pathfinder</a></p>
                 <p className="ap">Passionate about coding and learning new technology</p>
-                <p className="ap">Interested in working on meaningful projects and leaving the world better than I find it</p><br />
+                <p className="ap">Interested in working on meaningful projects and making the world a better place</p><br />
                 <h4 className="ah">Development and Technologies</h4>
                 <div className="icons">
                 <p className="ap iconEffect">JavaScript - React.js - Node.js - Angular - HTML & HTML5 - CSS & CSS3 - PostgreSQL - Bootstrap</p>
                 <p className="ap iconEffect">TypeScript - Express - Git - Heroku - MaterialUI - React-Native - Python </p><br /></div>
                 <h4 className="ah">Hobbies and Interests</h4>
-                <p className="ap">Meditation - Music - Yoga - Krav Maga - Podcasts - Economics - Philosophy - Psychology</p>
+                <p className="ap"><a className="nodecoration" href="https://happierunfound.home.blog" target="_blank">Writing - </a>Meditation - Music - Yoga - Krav Maga - Podcasts - Economics - Philosophy - Psychology</p>
                 <p className="ap">Meaningful Relationships - Meaningful Experiences</p>
             </div>
             <br /><br />
